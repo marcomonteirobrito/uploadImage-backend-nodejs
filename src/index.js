28abcd7d6bdc97d2cf7d40ccf33393d8c59e215b
Marco <marco_monteiro@live.com>
2020-07-09 14:06:05 -0300

    Corrige porta da aplicação

--- src/index.js
+++ src/index.js
@@ -20,4 +20,4 @@ app.use(morgan('dev'));
 app.use("/files", express.static(path.resolve(__dirname, ".." , "tmp", "uploads")));
 app.use(require('./routes'));
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
